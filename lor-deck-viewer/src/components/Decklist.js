26079d270daae37770687aa05c90b707a26299ab
wizracc <wizracc@gmail.com>
2020-02-16 10:31:48 -0600

    made things a bit better

--- lor-deck-viewer/src/components/Decklist.js
+++ lor-deck-viewer/src/components/Decklist.js
@@ -10,9 +10,11 @@ class Decklist extends Component {
       code: this.props.match.params.code,
       deck: [],
       hovered: null,
-      valid: false
+      valid: false,
+      images: {}
     };
     this.fetchDeck = this.fetchDeck.bind(this);
+    this.preloadImages = this.fetchImages.bind(this);
     this.handleHover = this.handleHover.bind(this);
     this.handleLeave = this.handleLeave.bind(this);
     this.fetchDeck();
@@ -31,6 +33,18 @@ class Decklist extends Component {
           valid: data.deck.length !== 0
         })
       );
+  }
+
+  fetchImages() {
+    const deck = this.state.deck;
+    let object = {};
+    for (let card of deck) {
+      let code = card.code;
+      let image = new Image();
+      image.src = `http://decks.wizra.cc/image/${code}`;
+      object = { ...object, [code]: image };
+    }
+    this.setState({ ...this.state, images: object });
   }
 
   handleHover(id) {
@@ -56,9 +70,7 @@ class Decklist extends Component {
       <div>
         <Header />
         <div className="Decklist">
-          {!this.state.valid && (
-            <p className="error">Invalid deck code</p>
-          )}
+          {!this.state.valid && <p className="error">Invalid deck code</p>}
           <div>
             {this.state.deck.map(cardInfo => (
               <Minicard
@@ -73,13 +85,14 @@ class Decklist extends Component {
             ))}
           </div>
           <div>
-            {this.state.hovered !== null && (
+            {this.state.deck.map(cardInfo => (
               <img
-                src={`http://decks.wizra.cc/image/${this.state.hovered}`}
-                alt={this.state.hovered}
-                fluid="true"
+                src={`http://decks.wizra.cc/image/${cardInfo.code}`}
+                className={
+                  cardInfo.code === this.state.hovered ? "visible" : "hidden"
+                }
               />
-            )}
+            ))}
           </div>
         </div>
       </div>
